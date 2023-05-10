b19d99a26b88b815198da6f07d15358ded4154ce
giuliatognali <118864811+giuliatognali@users.noreply.github.com>
2023-05-10 16:40:37 +0200

    migrate - seeder

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,13 +3,12 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Laravel</title>
+        <title>Comics DC</title>
          @vite('resources/js/app.js')
     </head>
     <body>
         <div class="container">
-            <h1>Template Classe 89</h1>
-            <img src="{{ Vite::asset('resources/img/pluto.webp') }}" alt="" srcset="">
+            <h1>Comics</h1>
         </div>
 
     </body>
