679f1ba7b2cf79543417d2a3424c8e3fd52b5cdd
giuliatognali <118864811+giuliatognali@users.noreply.github.com>
2023-05-10 17:09:09 +0200

    controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Admin\ComicController;
+use App\Http\Controllers\Guest\PageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/* Route::get('/', function () {
     return view('home');
-});
+}); */
+Route::get('/', [PageController::class, 'index']);
+Route::resource('comics', ComicController::class);
