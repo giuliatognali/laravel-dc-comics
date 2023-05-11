f124203f27b345a9f638bccfd95d910e6a274571
giuliatognali <118864811+giuliatognali@users.noreply.github.com>
2023-05-11 16:49:15 +0200

    delete route

--- resources/views/comic/show.blade.php
+++ resources/views/comic/show.blade.php
@@ -18,10 +18,15 @@
         <p>{{$comic->artists}}</p>
         <p>{{$comic->writers}}</p>
 
-    <div>
-        <a href="{{route('comics.index', $comic)}}"><button class="btn btn-primary">Torna all'elenco comics</button></a>
-        <a href="{{route('comics.edit', $comic)}}"><button class="btn btn-warning">Modifica i dettagli</button></a>
-
+    <div class="d-flex justify-content-center my-5">
+        <a href="{{route('comics.index', $comic)}}" class="mx-2"><button class="btn btn-primary mx-2">Torna all'elenco comics</button></a>
+        <a href="{{route('comics.edit', $comic)}}" class="mx-2"><button class="btn btn-warning mx-2">Modifica i dettagli</button></a>
+        {{-- aggiungo il pulsante per cancellare, deve essere un form con @method DELETE --}}
+        <form action="{{route('comics.destroy', $comic)}}" method="POST" class="mx-2">
+            @csrf
+            @method('DELETE')
+            <input type="submit" class="btn btn-danger mx-2" value="Cancella">
+        </form>
     </div>
 </div>
 @endsection
